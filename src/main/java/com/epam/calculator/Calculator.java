86136d553a17592d18ab853cf08c6962c0bcc19e
Анна Гришкевич <anna.gris06@mail.ru>
2021-11-24 18:33:43 +0300

    initial commit

--- src/main/java/com/epam/calculator/Calculator.java
+++ src/main/java/com/epam/calculator/Calculator.java
@@ -1,4 +1,20 @@
 package com.epam.calculator;
 
 public class Calculator {
+
+    public int add(int first, int second) {
+        return first + second;
+    }
+
+    public int subtract(int first, int second) {
+        return first - second;
+    }
+
+    public int divide(int first, int second) {
+        if(first % second == 0){
+            return first/second;
+        } else {
+            return first % second;
+        }
+    }
 }
